51ef871cda3587d2ef9c34d993f022563b6cab2c
David Rust-Smith <david.rs@hotmail.com>
2013-05-23 17:11:29 -0400

    David's aiming code

--- DavidDuck.rb
+++ DavidDuck.rb
@@ -8,7 +8,9 @@ class DavidDuck
 	 @my_accel = 1
 	 @gun_turn_speed =10
 	 @turn_speed =3
+	 @found_enemy = 0
    end
+   
    
   def tick events
     turn_radar 1 if time == 0
@@ -17,15 +19,46 @@ class DavidDuck
 	
 	
 	@last_hit = time unless events['got_hit'].empty? 
-	if @last_hit && time - @last_hit < 20
-      @turn_speed = (-@turn_speed*4+2)
+	if @last_hit && time - @last_hit < 5
+		@turn_speed = [8,(@turn_speed+1)].max
+		say('hit')
+	elsif @last_hit && (time - @last_hit < 20)
+		@turn_speed = rand(2...4);
+		say('not hit')
+	end
+	if (time-@found_enemy>30 )
+		say(['no enemy ',time].join)
+		@gun_turn_speed = 4
+		@turn_speed = rand(1..5)		
+	end
+		
+	if(time%5==0)
+		@turn_speed =0
+	end
+	if x<=65
+		turn_speed = 5 
+		say('left edge')
+	elsif x>=(battlefield_width-65)
+		turn_speed = 5 
+		say('right edge')
+	elsif y>= (battlefield_height-65)
+		turn_speed = 5
+		say('bottom edge')
+	elsif y<=  65 
+		turn_speed = 3
+		say('top edge')
+	end
+	
+	if (! events['robot_scanned'].empty?)
+		fire 3 
+		say('found enemy!')
+		@gun_turn_speed = -@turn_speed	
+		@turn_speed = 3
+		#turn_radar -1
+		@found_enemy=time
 	end	
-
-    accelerate(@my_accel)
-	turn @turn_speed
-	turn_gun @gun_turn_speed
-	
-    fire 3 unless events['robot_scanned'].empty? 
-	
+		accelerate(@my_accel)
+		turn @turn_speed
+		turn_gun @gun_turn_speed
   end
 end
